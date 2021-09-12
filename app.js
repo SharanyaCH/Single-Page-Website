bb041701e2093e243979108bdf4c2f185c2d7f75
SharanyaCH <73130507+SharanyaCH@users.noreply.github.com>
2021-09-12 19:57:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,22 +21,6 @@ const highlightMenu = () => {
   let scrollPos = window.scrollY;
   // console.log(scrollPos);
 
-  // adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 1400) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  }
-
   if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
     elem.classList.remove('highlight');
   }
